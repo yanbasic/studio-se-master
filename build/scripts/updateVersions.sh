13f463d70c1b0e644eaa48bb06276c41ba7b61ae
Irene Wang <iwang@talend.com>
2014-10-21 15:54:34 +0200

    Build related scripts

--- build/scripts/updateVersions.sh
+++ build/scripts/updateVersions.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-cd ../../talend.studio.parent.pom
-mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:0.21.0:set-version -DnewVersion=5.6.0-SNAPSHOT
-cd ..
-mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:0.21.0:set-version -DnewVersion=5.6.0-SNAPSHOT -Dartifacts=tos,org.talend.studio.tos.bigdata.product,org.talend.studio.tos.di.product,org.talend.studio.tos.dq.product,org.talend.studio.tos.esb.product,org.talend.studio.tos.mdm.product,tbd-studio-se,tcommon-studio-se,tdi-studio-se,tdq-studio-se,tesb-studio-se,tmdm-common,tmdm-studio-se,toem-studio-se
+cd ../../
+mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:0.21.0:set-version -DnewVersion=6.0.0-SNAPSHOT -Dartifacts=parent-pom,tos-products-parent-pom,tos,org.talend.studio.tos.bigdata.product,org.talend.studio.tos.di.product,org.talend.studio.tos.dq.product,org.talend.studio.tos.esb.product,org.talend.studio.tos.mdm.product,tbd-studio-se,tcommon-studio-se,tdi-studio-se,tdq-studio-se,tesb-studio-se,tmdm-common,tmdm-studio-se,toem-studio-se
